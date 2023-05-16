b0d4344e30927160ef56120bc0e2d6e43c640f68
Masoumeh Rezai <you@example.com>
2023-05-17 02:23:33 +0430

    workon style char bar page

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './ChartBar.css';
 const ChartBar = () => {
-    return
+    return (
+        <div className="chart-bar">
+            <div className="chart-bar__inner">
+                <div className="chart-bar__fill"></div>
+                <div className="chart-bar__lable"></div>
+            </div>
+        </div>
+    )
 }
 export default ChartBar;
