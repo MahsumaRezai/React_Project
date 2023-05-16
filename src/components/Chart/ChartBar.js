17db9407048fa5854f1d4afe1786515e9f654822
Masoumeh Rezai <you@example.com>
2023-05-17 02:38:30 +0430

    work on EXpensesChart page

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './ChartBar.css';
-const ChartBar = () => {
+const ChartBar = (props) => {
+    let barfillHeight = '0%';
+    if (props.max > 0) {
+        barfillHeight = Math.round((props.value / props.maxValue) * 100 + '%')
+
+    }
     return (
         <div className="chart-bar">
             <div className="chart-bar__inner">
-                <div className="chart-bar__fill"></div>
-                <div className="chart-bar__lable"></div>
+                <div className="chart-bar__fill" style={{ height: barfillHeight }}></div>
+                <div className="chart-bar__lable">{props.lable}</div>
             </div>
         </div>
     )
---
+++ src/components/Expenses/EXpensesChart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Chart from '../Chart/Chart.js';
+const ExpensesChart = props => {
+    return (
+        <Chart
+        />
+    )
+
+}
+export default ExpensesChart;
