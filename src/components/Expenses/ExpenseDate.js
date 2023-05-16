6e4493a93df5e6e88b0d61781ac544fb979fa091
Masoumeh Rezai <you@example.com>
2023-05-17 03:34:10 +0430

    last commit

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import './ExpenseDate.css';
-
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
