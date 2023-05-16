850a6b9d52c774d88f2f56a8d9467ca91ee1f231
Masoumeh Rezai <you@example.com>
2023-05-17 02:19:37 +0430

    add data in ChartBar file

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -5,6 +5,16 @@ const Chart = (props) => {
     return (
         <div className="chart">
             {props.dataPoints.map((dataPoint) => {
+                <ChartBar
+                    key={dataPoint.lable}
+                    value={dataPoint.value}
+                    maxValue={null}
+                    lable={dataPoint.lable}
+
+
+
+
+                />
 
             })}
 
---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './ChartBar.css';
+const ChartBar = () => {
+    return
+}
+export default ChartBar;
